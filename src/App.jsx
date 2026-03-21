7e39c2c22b526b2adaa816d1cc0068cf4dfe5e6f
juhairulislam <juhairulislam2018@gmail.com>
2026-03-21 14:44:51 +0600

    try to push in eid day

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+   const [input, setInput] = useState('') ;
 
   return (
+
+   
     <>
 
-    <h1 className='text-red-300'>Hello world</h1>
-    <div>
-      <button className='btn btn-accent'>Btn</button>
-    </div>
-  
+      <div>
+        <h1>My ToDo App</h1>
+
+        <input value={input} onChange={(e) => setInput(e.target.value)} placeholder='Type a task..' type="text" />
+
+        <p>You typed : {input}</p>
+      </div>
+
     </>
   )
 }
